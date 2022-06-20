d3117e13f37a836cd78351416279b334287eb722
OliveiraNadia <nadia.oliveira.n@outlook.com>
2022-06-20 08:21:37 -0300

    iniciando os testes unitarios

---
+++ api-fanfareInvest/src/api-fanfareInvest.test/Service/FixedIncomeServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace api_fanfareInvest.test.Service
+{
+    public class FixedIncomeServiceTest
+    {
+        public void Should_Success_When_ReturnGetFixedIncome()
+        {
+
+        }
+    }
+}
